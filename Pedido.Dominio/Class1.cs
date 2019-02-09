771cae72871979f02f5a72921ede0d9530d8a365
Douglas de Campos Held <dcheld@hotmail.com>
2019-02-08 23:46:13 -0200

    Adicionando a versao inicial do sistema

---
+++ Pedido.Dominio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Pedido.Dominio
+{
+    public class Class1
+    {
+    }
+}
---
+++ Pedido.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Pedido.Infra
+{
+    public class Class1
+    {
+    }
+}
